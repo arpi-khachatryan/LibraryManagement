b8cccb3c2ddb0eb06d7423d3e927d379444a69da
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-08-14 16:08:54 +0400

    BookStorage

--- src/main/java/storage/BookStorage.java
+++ src/main/java/storage/BookStorage.java
@@ -1,6 +1,14 @@
 package storage;
 
 import model.Book;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class BookStorage {
     private Book[] booksArray = new Book[10];
@@ -71,6 +79,59 @@ public class BookStorage {
         if (number == 0) {
             System.out.println("Sorry, there is no book at that price");
         }
+    }
+
+    public void createBooksExcel(String path) throws Exception {
+        File directory = new File(path);
+        if (directory.isFile()) {
+            throw new RuntimeException("file must be directory");
+        }
+        File excelFile = new File(directory, "book_" + System.currentTimeMillis() + ".XLSX");
+        excelFile.createNewFile();
+
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Book");
+
+        Row row = sheet.createRow(0);
+
+        Cell cell = row.createCell(0);
+        cell.setCellValue("No");
+
+        Cell cell2 = row.createCell(1);
+        cell2.setCellValue("Title");
+
+        Cell cell3 = row.createCell(2);
+        cell3.setCellValue("Author");
+
+        Cell cell4 = row.createCell(3);
+        cell4.setCellValue("Count");
+
+        Cell cell5 = row.createCell(4);
+        cell5.setCellValue("Genre");
+
+        Cell cell6 = row.createCell(5);
+        cell6.setCellValue("Price");
+
+        Cell cell7 = row.createCell(6);
+        cell7.setCellValue("Registered user");
+
+        Cell cell8 = row.createCell(7);
+        cell8.setCellValue("Registration date");
+
+        for (int i = 0; i < size; i++) {
+            Book book = booksArray[i];
+            Row row1 = sheet.createRow(i + 1);
+            row1.createCell(0).setCellValue(i + 1);
+            row1.createCell(1).setCellValue(book.getTitle());
+            row1.createCell(2).setCellValue(book.getAuthor().getName());
+            row1.createCell(3).setCellValue(book.getCount());
+            row1.createCell(4).setCellValue(book.getGenre());
+            row1.createCell(5).setCellValue(book.getPrice());
+            row1.createCell(6).setCellValue(book.getRegisteredUser().getName() + " " + book.getRegisteredUser().getSurname());
+            row1.createCell(7).setCellValue(book.getRegistrationDate().toString());
+        }
+        workbook.write(new FileOutputStream(excelFile));
+        System.out.println("Excel file was successfully created");
     }
 }
 
